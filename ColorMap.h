b87c2db24bd53286c41ec6b1baf7e8e686cfc1b2
sherinann <sherinannthomas1@gmail.com>
2025-05-24 14:41:32 +0530

    modified tests

---
+++ ColorMap.h
@@ -0,0 +1,7 @@
+#include <vector>
+#include <string>
+
+int PrintColorMap();
+std::vector<std::string> GetMajorColors();
+std::vector<std::string> GetMinorColors();
+std::string GenerateColorPairString(int majorColorIndex, int minorColorIndex);
